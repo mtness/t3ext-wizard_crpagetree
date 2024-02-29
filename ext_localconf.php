a6ae6551a7ad64e6ff7867867a9b35c194f9f28d
Markus Klein <markus.klein@reelworx.at>
2024-02-29 20:57:15 +0100

    [BUGFIX] Remove TYPO3_MODE check

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die('( ͡ಠ ʖ̯ ͡ಠ)╭∩╮');
+defined('TYPO3') or die('( ͡ಠ ʖ̯ ͡ಠ)╭∩╮');
 
 $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1555874765127] = \MichielRoos\WizardCrpagetree\ContextMenu\ItemProvider::class;
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['constructPostProcess'][] = \MichielRoos\WizardCrpagetree\Hook\BackendControllerHook::class . '->addJavaScript';
